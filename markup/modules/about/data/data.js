8015f6e005da84cf81e8ae1f55c835cf84ef8648
kopo4eweb <kopo4e.web@yandex.ru>
2016-03-08 17:37:00 +0300

    addition module About

---
+++ markup/modules/about/data/data.js
@@ -0,0 +1,9 @@
+about: {
+    defaults: {
+        title: 'About me',
+        text: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,</p>',
+        link_title: 'Read more',
+        link: '#',
+        img: 'about.jpg'
+    }
+}
