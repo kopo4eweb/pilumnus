e6bce75922f6ddf896026c9f6ee487b365620d58
kopo4eweb <kopo4e.web@yandex.ru>
2016-03-10 20:12:46 +0300

    add module Lastest

---
+++ markup/modules/lastest/data/data.js
@@ -0,0 +1,28 @@
+lastest: {
+    defaults: {
+        title: 'Lastest articles',
+        item: [
+            {
+                title: 'Lorem Ipsum Dolor',
+                img: 'article-1.jpg',
+                text: 'Lorem ipsum dolor sit amet, consectetur pisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.',
+                link: '#',
+                link_title: 'Read more'
+            },
+            {
+                title: 'Lorem Ipsum Dolor',
+                img: 'article-2.jpg',
+                text: 'Lorem ipsum dolor sit amet, consectetur pisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.',
+                link: '#',
+                link_title: 'Read more'
+            },
+            {
+                title: 'Lorem Ipsum Dolor',
+                img: 'article-3.jpg',
+                text: 'Lorem ipsum dolor sit amet, consectetur pisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.',
+                link: '#',
+                link_title: 'Read more'
+            }
+        ]
+    }
+}
