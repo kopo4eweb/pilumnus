5013a9b2213251f02dd2968ab80304797b3e6f6a
kopo4eweb <kopo4e.web@yandex.ru>
2016-03-11 18:06:29 +0300

    add Gallery module

---
+++ markup/modules/gallery/data/data.js
@@ -0,0 +1,44 @@
+gallery: {
+    defaults: {
+        title: 'Gallery',
+        item: [
+            {
+                title: '1',
+                img: 'gallery-1.jpg',
+                img_type: 1
+            },
+            {
+                title: '2',
+                img: 'gallery-2.jpg',
+                img_type: 1
+            },
+            {
+                title: '3',
+                img: 'gallery-3.jpg',
+                img_type: 2
+            },
+            {
+                title: '4',
+                img: 'gallery-4.jpg',
+                img_type: 2
+            },
+            {
+                title: '5',
+                img: 'gallery-5.jpg',
+                img_type: 2
+            },
+            {
+                title: '6',
+                img: 'gallery-6.jpg',
+                img_type: 2
+            },
+            {
+                title: '7',
+                img: 'gallery-7.jpg',
+                img_type: 2
+            }
+        ],
+        link: '#',
+        link_title: 'Show more'
+    }
+}
