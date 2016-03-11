aa1e68a6fde84304bfeabebb0147f89e161db0a9
kopo4eweb <kopo4e.web@yandex.ru>
2016-03-11 21:03:48 +0300

    add Contact module

---
+++ markup/modules/contact/contact.js
@@ -0,0 +1,31 @@
+
+(function () {
+    var form = document.getElementById('contact__form');
+    var requiredElements = form.querySelectorAll('[required]');
+
+    console.log(requiredElements);
+
+    for (var i = 0; i < requiredElements.length; i++) {
+        //console.log(requiredElements[i]);
+        requiredElements[i].required = false;
+    }
+
+    form.onsubmit = function (e) {
+        e.preventDefault();
+
+        //console.log('click button submit');
+        var countError = 0;
+        for (i = 0; i < requiredElements.length; i++) {
+            if (requiredElements[i].required === false) {
+
+                if (requiredElements[i].value.length > 0) {
+                    console.log(requiredElements[i].value);
+                } else {
+                    countError++;
+                }
+            }
+        }
+        console.log(countError);
+    };
+    // document.getElementById('input_name').required = false;
+})();
---
+++ markup/modules/contact/data/data.js
@@ -0,0 +1,11 @@
+contact: {
+    defaults: {
+        title: 'Contact',
+        link_title: 'Send message',
+        input_name: 'Your name',
+        input_email: 'Your email',
+        input_message: 'Message',
+        link: '#',
+        img: 'contact.jpg'
+    }
+}
