b4c2159b3a0055a05b5f2d6d827c8abc22d78cc8
kopo4eweb <kopo4e.web@yandex.ru>
2016-03-14 15:24:51 +0300

    add Follow module

---
+++ markup/modules/follow/data/data.js
@@ -0,0 +1,26 @@
+follow: {
+    defaults: {
+        item: [
+            {
+                title: 'facebook',
+                img: 'facebook-logo.svg',
+                link: '#'
+            },
+            {
+                title: 'twitter',
+                img: 'twitter-logo.svg',
+                link: '#'
+            },
+            {
+                title: 'instagram',
+                img: 'instagram-logo.svg',
+                link: '#'
+            },
+            {
+                title: 'pinterest',
+                img: 'pinterest-logo.svg',
+                link: '#'
+            }
+        ]
+    }
+}
