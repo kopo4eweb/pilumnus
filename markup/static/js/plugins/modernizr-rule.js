0cc8d2ae5e5a92bef739f6635e0256f13e3f6cbd
kopo4eweb <kopo4e.web@yandex.ru>
2016-03-31 20:41:03 +0300

    add plugin modernizr, replase svg analog png img

---
+++ markup/static/js/plugins/modernizr-rule.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    // SVG Fallback
+    if (!Modernizr.svg) {
+        $('img[src*="svg"]').attr('src', function () {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
+});
